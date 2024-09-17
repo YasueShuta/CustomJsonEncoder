b66bdb47321a07ac6919c445fd9d80cd20e45851
YasueShuta <shuta.59074.orionsbelt@gmail.com>
2024-09-17 17:50:44 +0900

    rename customizable_json_encoder.py
    rename CustomJSONEncoder to CJEnc

--- src/cjenc/customizable_json_encoder.py
+++ src/cjenc/customizable_json_encoder.py
@@ -12,78 +12,22 @@ from json.encoder import (
     JSONEncoder
 )
 CUSTOM_CLASSES = set()
+"""set of custom classes. if you add your custom classes beforehand,
+CJEnc custom_classes parameter is not required."""
 
-class CustomJSONEncoder(JSONEncoder):
-    """Extensible JSON <https://json.org> encoder for Python data structures.
+class CJEnc(JSONEncoder):
+    """CJEnc stands for Customizable JSON Encoder."""
 
-    Supports the following objects and types by default:
-
-    +-------------------+---------------+
-    | Python            | JSON          |
-    +===================+===============+
-    | dict              | object        |
-    +-------------------+---------------+
-    | list, tuple       | array         |
-    +-------------------+---------------+
-    | str               | string        |
-    +-------------------+---------------+
-    | int, float        | number        |
-    +-------------------+---------------+
-    | True              | true          |
-    +-------------------+---------------+
-    | False             | false         |
-    +-------------------+---------------+
-    | None              | null          |
-    +-------------------+---------------+
-
-    To extend this to recognize other objects, subclass and implement a
-    ``.default()`` method with another method that returns a serializable
-    object for ``o`` if possible, otherwise it should call the superclass
-    implementation (to raise ``TypeError``).
-
-    Add a custom class as custom_classes. Override encode method for serialize.
-    """
     def __init__(self, *, skipkeys=False, ensure_ascii=True,
             check_circular=True, allow_nan=True, sort_keys=False,
             indent=None, separators=None, default=None, custom_classes=[]):
-        """Constructor for JSONEncoder, with sensible defaults.
+        """Constructor for CJEnc.
 
-        If skipkeys is false, then it is a TypeError to attempt
-        encoding of keys that are not str, int, float or None.  If
-        skipkeys is True, such items are simply skipped.
+        Parameters:
+            custom_classes - add custom class which extends cjenc.CustomSerializable class.
+                             more detail in help(cjenc.CustomSerializable).
 
-        If ensure_ascii is true, the output is guaranteed to be str
-        objects with all incoming non-ASCII characters escaped.  If
-        ensure_ascii is false, the output can contain non-ASCII characters.
-
-        If check_circular is true, then lists, dicts, and custom encoded
-        objects will be checked for circular references during encoding to
-        prevent an infinite recursion (which would cause an RecursionError).
-        Otherwise, no such check takes place.
-
-        If allow_nan is true, then NaN, Infinity, and -Infinity will be
-        encoded as such.  This behavior is not JSON specification compliant,
-        but is consistent with most JavaScript based encoders and decoders.
-        Otherwise, it will be a ValueError to encode such floats.
-
-        If sort_keys is true, then the output of dictionaries will be
-        sorted by key; this is useful for regression tests to ensure
-        that JSON serializations can be compared on a day-to-day basis.
-
-        If indent is a non-negative integer, then JSON array
-        elements and object members will be pretty-printed with that
-        indent level.  An indent level of 0 will only insert newlines.
-        None is the most compact representation.
-
-        If specified, separators should be an (item_separator, key_separator)
-        tuple.  The default is (', ', ': ') if *indent* is ``None`` and
-        (',', ': ') otherwise.  To get the most compact JSON representation,
-        you should specify (',', ':') to eliminate whitespace.
-
-        If specified, default is a function that gets called for objects
-        that can't otherwise be serialized.  It should return a JSON encodable
-        version of the object or raise a ``TypeError``.
-
+        other parameters derives from json.JSONEncoder.
         """
 
         super().__init__(skipkeys=skipkeys,
@@ -125,15 +69,6 @@ class CustomJSONEncoder(JSONEncoder):
         return ''.join(chunks)
 
     def iterencode(self, o, _one_shot=False):
-        """Encode the given object and yield each string
-        representation as available.
-
-        For example::
-
-            for chunk in JSONEncoder().iterencode(bigobject):
-                mysocket.write(chunk)
-
-        """
         if self.check_circular:
             markers = {}
         else:
